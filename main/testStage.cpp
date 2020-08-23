51894b47c42569fae79f5a0a7c4bda086b111cb0
Karsten Becker <kb@karsten-becker.de>
2020-08-23 12:29:18 +0200

    Commiting test-stage file

---
+++ main/testStage.cpp
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2020 Karsten Becker All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+#include <iostream>
+
+int main() { return 0; }
